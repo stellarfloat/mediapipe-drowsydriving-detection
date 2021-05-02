c6f8650ffea107a44d5fc53ca30f84f6feff3d53
stellarfloat <66378218+stellarfloat@users.noreply.github.com>
2021-05-02 11:49:41 +0900

    Add index visualizer for image

--- landmark_coors_test.py
+++ landmark_coors_test.py
@@ -8,7 +8,7 @@ POS_EYE_L = []
 POS_MOUTH = []
 
 
-if __name__ == '__main__':
+def mark_index_webcam():
     mp_drawing = mp.solutions.drawing_utils
     mp_face_mesh = mp.solutions.face_mesh
 
@@ -63,3 +63,54 @@ if __name__ == '__main__':
         
         cv2.destroyAllWindows()
         cap.release()
+
+
+def mark_index_image(img_path: str, output_path: str):
+    mp_drawing = mp.solutions.drawing_utils
+    mp_face_mesh = mp.solutions.face_mesh
+
+    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+    with mp_face_mesh.FaceMesh(
+    min_detection_confidence=0.35,
+    min_tracking_confidence=0.65) as face_mesh:
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = face_mesh.process(image)
+
+        if results.multi_face_landmarks:
+            # landmark
+            landmark_list = []
+            # normalized coordinates, 
+            # ref: stackoverflow.com/questions/67141844/how-do-i-get-the-coordinates-of-face-mash-landmarks-in-mediapipe
+            for fp in results.multi_face_landmarks[0].landmark:
+                landmark_list.append([fp.x, fp.y, fp.z])
+            
+            # denormalize
+            for i in range(len(landmark_list)):
+                landmark_list[i][0] *= image.shape[1]
+                landmark_list[i][1] *= image.shape[0]
+            
+            # Draw the face mesh annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACE_CONNECTIONS,
+                        landmark_drawing_spec=drawing_spec,
+                        connection_drawing_spec=drawing_spec)
+            
+            for i in range(len(landmark_list)):
+                pos = (int(landmark_list[i][0]), int(landmark_list[i][1]))
+                cv2.putText(image, f'{i}', pos, cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255,255,255), 1, cv2.LINE_AA)
+            
+            cv2.imwrite(output_path, image)
+
+
+if __name__ == '__main__':
+    #mark_index_webcam()
+    mark_index_image('./joseph-gonzalez-iFgRcqHznqg-unsplash.jpg', './face_img_index.png')
